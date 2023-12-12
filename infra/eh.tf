a837f3a1f0fdbc4a8a5e6ceb2038f5605ce0aced
Edgar Bahilo Rodríguez <edgar.bahilorodriguez@gmail.com>
2023-12-12 14:44:30 +0100

    Create eh.tf

---
+++ infra/eh.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_eventhub_namespace" "example" {
+  name                = "acceptanceTestEventHubNamespace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+resource "azurerm_eventhub" "example" {
+  name                = "acceptanceTestEventHub"
+  namespace_name      = azurerm_eventhub_namespace.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+
+resource "azurerm_eventhub_namespace_schema_group" "test" {
+  name                 = "example-schemaGroup"
+  namespace_id         = azurerm_eventhub_namespace.test.id
+  schema_compatibility = "Forward"
+  schema_type          = "Avro"
+}
